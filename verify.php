3bc1ec50bf0919c3c0375b0472dc4a2966d2b576
ChanaweeK <43096352+ChanaweeK@users.noreply.github.com>
2018-10-27 17:12:55 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'N/h001PQ4GZ/ocMxZztKtM4bPDSJopxKjviFIcWGl0muLEyfCLMgxAZyxFjn47eeoVKhifapyxUqeTCcwsgp+YJvc2wj427vDfYt6fzfTWAFPJ8yRPZ6qn/ZiyynBWBLh/t8h7eGFh5Mw/H0yESV9wdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 

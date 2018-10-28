9e943ba3de3baca2aa488d6d0e363858d810140d
ChanaweeK <43096352+ChanaweeK@users.noreply.github.com>
2018-10-28 13:20:43 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'naychanawee',
+        'to' => 'Ub358137ef653e469b05224eb48ad6cf2',
         'messages' => [$messages],
       ];
       $post = json_encode($data);

56a7735731ba6e2b6664f8127c0a5eb1cba651b1
ChanaweeK <43096352+ChanaweeK@users.noreply.github.com>
2018-10-30 21:15:08 +0700

    Update bottest.php

--- bottest.php
+++ bottest.php
@@ -54,6 +54,11 @@
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else if($message == "on"){
+       
+	getMqttfromlineMsg("NodeMCU1","1");
+        
+    }
 function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
         $ch = curl_init();
